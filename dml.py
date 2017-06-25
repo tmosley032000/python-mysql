bd5011f759266009f7fc9a6baa8f0e0ebf6b6a24
Thomas Mosley <thomasmosley@Thomass-MacBook-Pro.local>
2017-06-25 16:15:47 -0700

    added bind variables

--- dml.py
+++ dml.py
@@ -1,9 +1,6 @@
 def insert_rec(cur, dbh, insert_vals):
   query = "INSERT INTO tablename(id,name)" "VALUES(%s,%s)" 
-  #args =(900,"foo")
-  #cur.execute(query,args)
   cur.execute(query,insert_vals)
-  
   dbh.commit()
 
 def delete_rec(cur, dbh):
