3abd80a98046f589b3d0e7842ee499be10872682
Thomas Mosley <thomasmosley@Thomass-MacBook-Pro.local>
2019-04-11 15:03:19 -0700

    added comments

--- test.py
+++ test.py
@@ -14,17 +14,17 @@ from random import randint
 
 
 #
-# Assumptions: 
+# Assumptions:
 # 1. you have installed mysql.connector
 # 2. you have a running Mysql DB that can be accessed
 # 3. you have changed the db_config.py *OR* db_conig.json file to point to your DB
 # 4. you have changed/added your table name to the db_config.py or db_config.json file pointing to your table *Use only one, it's your choice
 #
-
-# 
+# added more comments
+#
 #for using json (db_config.json) file for connection:
 #  "db_config":
-#              {"user":"tmosley","password":"tmosley","host":"localhost","database":"twith open('db_config.json') as data_file:    
+#              {"user":"tmosley","password":"tmosley","host":"localhost","database":"twith open('db_config.json') as data_file:
 with open('db_config.json') as data_file:
     data = json.load(data_file)
 
@@ -66,11 +66,11 @@ db_cursor.execute(create_table_sample())
 # return_random = get_random.return_random()
 # insert_rec = dml.insert_rec()
 
-# table_names is a tablename config found in table_name dict of db_config.py or db_config.json ** Use only one, its your choce 
+# table_names is a tablename config found in table_name dict of db_config.py or db_config.json ** Use only one, its your choce
 # dict - table_names = {'tablename':'tablename',
 #                 'foobar':'foobar'
 #                }
-#         return table_names 
+#         return table_names
 # json -   "table_names":
 #             {"tablename":"tablename"}
 #
@@ -89,31 +89,31 @@ with open("datafile.txt", "r") as open_file:
 
 
 #query all records queries.all_records()
-result_cursor = execute_query(db_cursor,all_records()) 
+result_cursor = execute_query(db_cursor,all_records())
 
 # The following is just there to query the db and verify the contents, pretty much throw away code ;)
 #some code to verify db contents
 print result_cursor.column_names
 
 all_recs = result_cursor.fetchall()
- 
+
  #
  # all_recs is an array of arrays: [[]] <- array of arrays i.e
- # [(1194, u'test4\n'), (1996, u'foobar'), (2577, u'test1\n'), 
+ # [(1194, u'test4\n'), (1996, u'foobar'), (2577, u'test1\n'),
  #  (4858, u'test5\n'), (5316, u'test2\n'), (9766, u'test3\n')]
  #
 
 i = 0
 for rec in all_recs:
- 
+
   #
   # iterate over all records
-  # rec is one of the elements of all_arrays [], also an array [],but now at the data i.e 
-  # (1194, u'test4\n') ** the first record, just an example, see above 
+  # rec is one of the elements of all_arrays [], also an array [],but now at the data i.e
+  # (1194, u'test4\n') ** the first record, just an example, see above
   #
 
   print "record #->", i,  "id ->",rec[0] ,"name->",rec[1].decode('ascii', 'ignore').strip()
-    
+
     #
     # index is the record index, id is the column id(rec[0]), name is the column name(rec[1])
     # mysql> desc tablename;
